b785832c6d6d12d64f050f921210bb08e66550b4
tlgman <raf214@hotmail.fr>
2014-11-26 17:26:42 +0100

    Merge origin/master
    Conflicts:
    	.gitignore
    	src/main.java

--- src/main.java
+++ src/main.java
@@ -3,11 +3,8 @@ import javax.swing.UnsupportedLookAndFeelException;
 import view.*;
 
 public class main {
-
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         MainScreen MScreen = new MainScreen();
-
-
     }
 }
