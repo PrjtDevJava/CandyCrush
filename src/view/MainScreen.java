1898af774bd8d673ca445ab3bcf93a8ee70a0a7a
tlgman <raf214@hotmail.fr>
2014-11-26 15:05:40 +0100

    ....

---
+++ src/view/MainScreen.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Sylvio
+ */
+public class MainScreen extends JFrame{
+    public MainScreen(){
+        this.setTitle("Candy Crush 2.0");
+        this.setSize(600, 700);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
+        this.setVisible(true);
+    }
+    
+    
+    
+}
