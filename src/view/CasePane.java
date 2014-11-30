7411c858af5f21810abe3a96b9a092b56af0d76e
tlgman <raf214@hotmail.fr>
2014-11-30 22:22:25 +0100

    Merge origin/master
    Conflicts:
    	.gitignore
    	src/main.java

--- src/view/CasePane.java
+++ src/view/CasePane.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Observable;
 import java.util.Observer;
 import javax.swing.JPanel;
@@ -9,10 +12,31 @@ import model.Shape;
 public class CasePane extends JPanel implements Observer {
 
     public CasePane() {
-
     }
-
     public void init(Shape shape) {
+        this.addMouseListener(new MouseListener(){
+            @Override
+            public void mouseClicked(MouseEvent me) {
+                System.out.println("Click !!!");
+                CasePane.this.setBackground(Color.black);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent me) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+            }
+        });
         this.setBackground(shape.getColor());
     }
 
@@ -20,6 +44,7 @@ public class CasePane extends JPanel implements Observer {
     public void update(Observable obs, Object obj) {
         if (obs instanceof Case) {
             Case c = (Case) obs;
+            this.setBackground(c.getShape().getColor());
         }
     }
 
